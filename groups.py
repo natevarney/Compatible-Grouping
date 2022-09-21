28c03ec987225f2f5d82aba257c978d4ede329f4
natevarney <nvarney@umass.edu>
2022-09-21 11:47:38 -0400

    Functions to generate people and load into groups

--- groups.py
+++ groups.py
@@ -1,12 +1,12 @@
 from array import array
 import people
 
+allGroups = set()
+
 class Groups:
-    def __init__(self, groupsSet:set = set(), maxNumGroups:int = 0):
-        global allGroups
-        allGroups = groupsSet
+    def __init__(self, maxNumGroups:int = 0):
         self.maxNumGroups = maxNumGroups
-        self.currentNumGroups = len(groupsSet)
+        self.currentNumGroups = len(allGroups)
 
         if self.currentNumGroups > self.maxNumGroups:
             raise Exception("Inputted group set exceeds maximum number of total groups.")
@@ -37,11 +37,11 @@ class Group:
             raise Exception("Inputted group exceeds maximum number of people allowed in a group")
 
     def addPersonToGroup(self,person:people.Person):
-        # if len(self.group) < self.maxGroupSize:
-        self.group.add(person)
-        #     self.currentGroupSize += 1
-        # else:
-        #     print("Group is full, person could not be added to group.")
+        if len(self.group) < self.maxGroupSize:
+            self.group.add(person)
+            self.currentGroupSize += 1
+        else:
+            print("Group is full, person could not be added to group.")
 
     def removePersonFromGroup(self,person:people.Person):
         if person in self.group:
--- main.py
+++ main.py
@@ -1,30 +1,74 @@
+from array import array
+from tokenize import group
 import people
 import groups
+import random
+import pprint as pp
+
+
+def generateRandomPeople(namesList:array):
+    numPeople = len(namesList)
+    questionAnswersList = []
+
+    for _ in range(numPeople):
+        answers = []
+        for _ in range(5):
+            answers.append(random.randint(0, 1))
+        questionAnswersList.append(answers)
+
+    for name,answers in zip(namesList,questionAnswersList):
+        person = people.Person(name, -1, answers)
+        people.peopleSet.add(person)
+
+def loadPeopleIntoGroups(maxGroupSize:int,maxNumGroups:int,groupsObj:groups.Groups):
+    while len(groups.allGroups) < maxNumGroups and people.peopleSet:
+            newGroup = groups.Group(set(),maxGroupSize)
+            while len(newGroup.group) < maxGroupSize and people.peopleSet:
+                    currentPerson = people.peopleSet.pop()
+                    newGroup.addPersonToGroup(currentPerson)
+                    groupNumber = newGroup.groupNumber
+                    currentPerson.currentGroup = groupNumber
+            groupsObj.addGroup(newGroup)
+    if len(people.peopleSet) > 0:
+        raise Exception("More people exist in people set, than total allowed to fit into groups.\n The maximum number of people you can have is: "+ str(maxGroupSize*maxNumGroups))
+
+    
+
 
 
 def main():
+    testNames = ['Liam','Noah','Oliver','Elijah','James','William','Benjamin','Lucas','Henry','Theodore','Jack','Levi','Alexander','Jackson','Mateo','Daniel']
+    maxGroupSize = 4
+    maxNumGroups = 4
     # jack = people.Person("Jack",14,[1,1,1,1,1])
     # jack.updateName("Bob")
     # jack.addPersonToPeopleArray()
     # print(people.peopleArray)
     # print([vars(node) for node in people.peopleArray])
 
-    groupsObj = groups.Groups(set(),maxNumGroups=4)
+    groupsObj = groups.Groups(maxNumGroups=4)
 
-    for _ in range(16):
-        newPerson = people.Person("bob",-1,[1,1,1])
-        people.peopleSet.add(newPerson)
+    # for _ in range(16):
+    #     newPerson = people.Person("bob",-1,[1,1,1])
+    #     people.peopleSet.add(newPerson)
 
-    groups.allGroups.clear()
-    for _ in range(4):
-        newGroup = groups.Group(set(),4)
-        while len(newGroup.group) < 4 and len(people.peopleSet) > 0:
-            currentPerson = people.peopleSet.pop()
-            newGroup.addPersonToGroup(currentPerson)
-            groups.allGroups.add(newGroup)
+    # groups.allGroups.clear()
+    # for _ in range(4):
+    #     newGroup = groups.Group(set(),4)
+    #     while len(newGroup.group) < 4 and len(people.peopleSet) > 0:
+    #         currentPerson = people.peopleSet.pop()
+    #         newGroup.addPersonToGroup(currentPerson)
+    #         groups.allGroups.add(newGroup)
 
-    randomGroup = list(groups.allGroups)[0]
-    print(vars(list(randomGroup.group)[0]))
+    # randomGroup = list(groups.allGroups)[0]
+    # print(vars(list(randomGroup.group)[0]))
+
+    generateRandomPeople(testNames)
+    loadPeopleIntoGroups(maxGroupSize,maxNumGroups,groupsObj)
+
+    pp.pprint(vars(list(groups.allGroups)[0]))
+    pp.pprint(list(list(groups.allGroups)[0].group)[0].currentGroup)
+    #pp.pprint([vars(person) for person in list(people.peopleSet)])
 
 if __name__=="__main__":
     main()
